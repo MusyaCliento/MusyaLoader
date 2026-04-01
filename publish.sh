2b8f1839805709aff991318f6c294e78e8d676db
depsad <scorpion95445@gmail.com>
2026-04-01 11:47:11 +0400

    linux: half of the build fix

--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
-python .\publish.py windows --x64-only
-python .\publish.py linux --x64-only
-python .\publish.py osx --x64-only
+python ./publish.py windows --x64-only
+python ./publish.py linux --x64-only
+python ./publish.py osx --x64-only
